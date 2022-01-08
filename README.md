e0a0c675d0c82561ee4dd5fb7d1402dca9ad9004
chin <1277180540@qq.com>
2022-01-08 17:22:27 +0800

    finish: dropwown and columnlist

--- README.md
+++ README.md
@@ -12,3 +12,7 @@ https://whimsical.com/vue3-Djb2TcWsLTPeapFdM3NaX
 - 在线后端 API 查询和使用站点：http://api.vikingship.xyz/ 
 - 项目在线文档：http://docs.vikingship.xyz/ 
 - 完成的组件库展示：http://showcase.vikingship.xyz/
+
+## 老师的研发规范
+
+https://imooc-lego.yuque.com/books/share/a8088dbe-5952-445c-982d-3808da620795
---
+++ src/components/DropdownItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <li
+    class="dropdown-option"
+    :class="{'is-disabled': disabled}"
+  >
+    <slot></slot>
+  </li>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  }
+})
+</script>
+
+<style>
+/* 中间不能用空格，要不就成子元素选择器了 */
+.dropdown-option.is-disabled * {
+  color: #6c757d;
+  pointer-events: none;
+  background-color: transparent;
+}
+</style>
